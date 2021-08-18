ec007ea4169a643a475c41ebf7c08d86fb9ab832
Simon Weis <laguna.ws@gmail.com>
2021-08-18 08:40:04 +0200

    Add python solution

--- python/src/secant.py
+++ python/src/secant.py
@@ -1,6 +1,16 @@
 def calculate_secant_slope(f, x0, x1):
-    return 42.0
+    return (f(x1) - f(x0)) / (x1 - x0)
 
 
 def secant(f, x0, x1, delta):
-    return 42.0
+    x = x1
+    xlast = x0
+    v = f(x)
+
+    while (abs(v) > delta):
+        d = calculate_secant_slope(f, x, xlast)
+        xlast = x
+        x = x - v / d
+        v = f(x)
+
+    return x
